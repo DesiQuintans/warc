e5a2d076b4cce13315d198b789c46862f8ae6832
Desi Joshua Quintans <science@desiquintans.com>
2023-07-10 10:48:49 +1000

    Add example for all cols

--- R/exp_report_missingness.R
+++ R/exp_report_missingness.R
@@ -3,16 +3,22 @@
 #'
 #' @param df (Dataframe) A dataframe.
 #' @param pattern (Character) A regular expression that will be used to select columns of `df`, passed to [agrep()].
+#' @param ignore.case (Logical) If `TRUE` (default), ignore case when matching.
 #' @param ... (Dots) Other arguments that will be passed to [agrep()].
 #'
 #' @return Prints to the console.
 #' @export
 #'
 #' @examples
-#' report_missingness(iris, "^Petal")
-#' report_missingness(iris, "Length$")
-report_missingness <- function(df, pattern, ...) {
-    out_names <- agrep(pattern = pattern, x = colnames(df), value = TRUE, ...)
+#' report_missingness(iris, "^petal")   # Starts with "petal"
+#' report_missingness(iris, "length$")  # Ends with "length"
+#' report_missingness(iris, ".")        # Matches anything (returns all cols)
+report_missingness <- function(df, pattern, ignore.case = TRUE, ...) {
+    out_names <- agrep(pattern = pattern,
+                       x = colnames(df),
+                       ignore.case = ignore.case,
+                       value = TRUE,
+                       ...)
 
     result_format <- sprintf("# %%-%is: %%.2f%%%% missing", max(nchar(out_names)))
 
